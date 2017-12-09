457ab11199e5a390f1897a368811579ed0ba76c3
Yan Ni <niyan.ny@pku.edu.cn>
2017-12-09 15:57:16 +0800

    first commit after fork

--- source_project/config.py
+++ source_project/config.py
@@ -6,18 +6,22 @@ machine = socket.gethostname()
 ROOT_DIR = os.path.dirname(__file__)
 print(ROOT_DIR)
 
-DUMPED_VECTOR_DIR_HL = '/raid/data/skar3/semeval/vectors_hl_new/'
-DUMPED_VECTOR_DIR = '/raid/data/skar3/semeval/vectors_mb_new/'
-TRUE_LABELS_PATH = '/raid/data/skar3/semeval/vectors_mb_new/mb_scores.pkl'
+DATA_DIR = '/home/niyan/SemEval17-05-kar/data'
+DUMPED_VECTOR_DIR_HL = os.path.join(DATA_DIR, 'vectors_hl_new/')
+DUMPED_VECTOR_DIR = os.path.join(DATA_DIR, 'vectors_mb_new/')
+TRUE_LABELS_PATH = os.path.join(DATA_DIR, 'vectors_mb_new/mb_scores.pkl')
 # TRUE_LABELS_PATH = '/raid/data/skar3/semeval/vectors/headline_scores.pkl'
 
 PROCESSED_DATA_DIR = os.path.join(ROOT_DIR, 'processed_data')
-RESULTS_DIR = os.path.join(ROOT_DIR, 'outputs/')
-WORD_EMBEDDING_VECTOR_PATH = '/raid/data/skar3/GoogleNews-vectors-negative300.bin.gz'
-VOCABULARY_WORD_TO_INDEX_LIST = '/raid/data/skar3/semeval/data/preprocessed/headline_vocabulary_to_index.json'
-VOCABULARY_INDEX_TO_WORD_LIST = '/raid/data/skar3/semeval/data/preprocessed/headline_index_to_word.json'
+RESULTS_DIR = os.path.join(DATA_DIR, 'outputs/')
+WORD_EMBEDDING_VECTOR_PATH = '/home/niyan/stockNN/data/wemb/GoogleNews-vectors-negative300.bin'
+VOCABULARY_WORD_TO_INDEX_LIST = os.path.join(DATA_DIR, 'preprocessed/headline_vocabulary_to_index.json')
+VOCABULARY_INDEX_TO_WORD_LIST = os.path.join(DATA_DIR, 'preprocessed/headline_index_to_word.json')
 
 features_to_extract = [
+    'rf_unigram',
+    'rf_bigram',
+    'rf_trigram',
     'unigram',
     'bigram',
     'trigram',
@@ -27,25 +31,25 @@ features_to_extract = [
     'char_tri',
     'char_4_gram',
     'char_5_gram',
-    'two_skip_3_grams',
-    'two_skip_2_grams',
+    # 'two_skip_3_grams',
+    # 'two_skip_2_grams',
     'concepts',
-    # 'stemmed_concepts',
+    'stemmed_concepts',
     'google_word_emb',
     'cashtag',
-    'source'
-    # 'polarity',
-    # 'sensitivity',
-    # 'attention',
-    # 'aptitude',
-    # 'pleasantness',
-    # 'stemmed_polarity',
-    # 'stemmed_sensitivity',
-    # 'stemmed_attention',
-    # 'stemmed_aptitude',
-    # 'stemmed_pleasantness',
+    'source',
+    'polarity',
+    'sensitivity',
+    'attention',
+    'aptitude',
+    'pleasantness',
+    'stemmed_polarity',
+    'stemmed_sensitivity',
+    'stemmed_attention',
+    'stemmed_aptitude',
+    'stemmed_pleasantness',
     # 'company'
-
+    'scores',
 ]
 
 features_to_use = [
@@ -83,16 +87,16 @@ features_to_use_mb = [
     'binary_unigram',
     'binary_bigram',
     'binary_trigram',
-    'char_tri',
-    'char_4_gram',
-    'char_5_gram',
-    'two_skip_3_grams',
-    'two_skip_2_grams',
-    'concepts',
-    'stemmed_concepts',
+    # 'char_tri',
+    # 'char_4_gram',
+    # 'char_5_gram',
+    # 'two_skip_3_grams',
+    # 'two_skip_2_grams',
+    # 'concepts',
+    # 'stemmed_concepts',
     'google_word_emb',
-    'cashtag',
-    'source',
+    # 'cashtag',
+    # 'source',
     'polarity',
     'sensitivity',
     'attention',
